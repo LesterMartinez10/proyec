9a50c144bc0a551cba48ca5b1f54f24bbd5736f8
Lester Orlando Martinez Hernández <201900062@colsanjose.edu.gt>
2020-05-15 09:37:47 -0600

    lester

--- ventas.php
+++ ventas.php
@@ -1,26 +1,21 @@
 <!doctype html>
 <html lang="en">
     <head>
-       
-
         <title>Ventas</title>
-    </head>
-        <body style="background: #f2f3f4">
+    </head>     
         <br />
         <div align="center">
             <h1>VENTAS</h1>
         </div>
-
         <div align="right">
             <a href="index.html">Salir</a>
         </div>
-        <hr />
         <br />
         <?php
             if(isset($_GET['producto']))
             {
                 include('conexion.php');
-                $con = new conexiono();
+                $con = new conexion;
                 $producto=$_GET['producto'];
                 $precio=$_GET['precio'];
 
@@ -72,53 +67,60 @@
                                 <th scope="col">Producto</th>
                                 <th scope="col">Descripcion</th>
                                 <th scope="col">Precio</th>
-                                <th scope="col">Agragar Carrito</th>
+                                <th scope="col">Agregar</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>
-                                    <img src="img/nintendoswich.jpg" />
+                                    <img src="img/nintendoswich.jpg"width="250" height="200"/>
                                 </td>
                                 <td>
                                     <p>
-                                        El Samsung Galaxy J3 Prime sale al mercado como uno de los nuevos modelos de gama de entrada del fabricante, con un precio que se sitúa en la órbita de los 100 euros y con el aliciente de debutar con Android 7.0 Nougat de serie.
+                                      Switch es diferente a todas las consolas que hemos visto hasta el momento.<br/>
+                                      El ultimo juguete de Nintendo es una maquina que tanto puede utilizarse conectada a la television y a una toma de corriente,<br/>
+                                      como de forma portatil gracias a una pantalla y bateria propias.<br/>
+                                      De hecho, casi podria decirse que en lugar de una consola se trata de una tableta pensada exclusivamente para jugar.<br/>                                 
                                     </p>
                                 </td>
                                 <td>
-                                    <p>$ 100.00</p>
+                                    <p>$350.00</p>
                                 </td>
                                 <td>
-                                    <a href="?producto=nintendoswich&precio=100">Comprar</a>
+                                    <a href="?producto=nintendoswich&precio=350">Comprar</a>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <img src="img/nintendoswichlite.jpg" />
+                                    <img src="img/nintendoswichlite.jpg"width="250" height="200"/>
                                 </td>
                                 <td>
                                     <p>
-                                        El Galaxy A50 es pura estética de primera calidad. Ya sea en sus colores modernos blanco o negro o en los colores brillantes coral o azul, su cuerpo delgado cabe cómodamente en la mano. Además, con la cubierta de plástico vidriado 3D y el escáner de huellas dactilares en la pantalla, sentirás curvas suaves y perfectas en los bordes.
+                                       Nintendo Switch Lite es la nueva incorporacion a la familia Nintendo Switch:<br/>
+                                       se trata de una consola compacta y ligera que se puede llevar a cualquier sitio con facilidad. <br/>
+                                       Con Nintendo Switch Lite se puede jugar a todos los programas de Nintendo Switch que son compatibles con el modo portatil<br/>
                                     </p>
                                 </td>
                                 <td>
-                                    <p>$ 150.00</p>
+                                    <p>$200.00</p>
                                 </td>
                                 <td>
-                                    <a href="?producto=nintendoswichlite&precio=150">Comprar</a>
+                                    <a href="?producto=nintendoswichlite&precio=200">Comprar</a>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <img src="img/xbox.jpg" />
+                                    <img src="img/xbox.jpg"width="250" height="200" />
                                 </td>
                                 <td>
                                     <p>
-                                        Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.
+                                        Ya sea que estes disfrutando de tus juegos, mirando tus series favoritas o haciendo streaming de tu juego,<br/> 
+                                        Xbox One S ofrece impresionantes graficos con tecnologia HDR,<br/>
+                                        audio de primera calidad y capacidad de juego rapido y confiable con tus amigos.<br/>
                                     </p>
                                 </td>
                                 <td>
-                                    <p>$ 250.00</p>
+                                    <p> $250.00</p>
                                 </td>
                                 <td>
                                     <a href="?producto=xbox&precio=250">Comprar</a>
@@ -126,31 +128,36 @@
                             </tr>
                             <tr>
                                 <td>
-                                    <img src="img/xboxx.jpg" />
+                                    <img src="img/xboxx.jpg"width="250" height="200" />
                                 </td>
                                 <td>
                                     <p>
-                                        Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.
+                                        Todos los juegos se ven y juegan bien en Xbox One X.<br/> 
+                                        Pero los titulos seleccionados estan optimizados para aprovechar la consola mas potente del mundo.<br/>
+                                        Estos juegos son para Xbox One X Enhanced.<br/>
                                     </p>
                                 </td>
                                 <td>
-                                    <p>$ 250.00</p>
+                                    <p>$450.00</p>
                                 </td>
                                 <td>
-                                    <a href="?producto=xboxx&precio=250">Comprar</a>
+                                    <a href="?producto=xboxx&precio=450">Comprar</a>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <img src="img/play.jpg" />
+                                    <img src="img/play.jpg"width="250" height="200" />
                                 </td>
                                 <td>
                                     <p>
-                                        Hemos diseñado Galaxy S7 y S7 edge dando rienda suelta a nuestra imaginación. El resultado es una usabilidad incomparable, repleta de funciones avanzadas con un aspecto descaradamente moderno. Unos smartphones con un aspecto realmente atractivo y muy fáciles de usar.
+                                        Presentamos la consola PlayStation 4.<br/>
+                                        Ahora con una construccion mas liviano y mas delgado que nunca<br/>
+                                        Disco duro de 1 TB<br/>
+                                        Todos los mejores juegos, programas de television, musica y mucho mas.<br/>
                                     </p>
                                 </td>
                                 <td>
-                                    <p>$ 250.00</p>
+                                    <p>             $250.00</p>
                                 </td>
                                 <td>
                                     <a href="?producto=play&precio=250">Comprar</a>
